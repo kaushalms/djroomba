302789daa1ba90adaa2d0d92399594191ed3b59f
kmandayam <kmandayam@apppartner.com>
2017-04-29 16:35:02 -0400

    Add manager classes

---
+++ DJRoomba/app/src/main/java/com/kaushalmandayam/djroomba/managers/AudioPlayerManager.java
@@ -0,0 +1,12 @@
+package com.kaushalmandayam.djroomba.managers;
+
+/**
+ * Singleton to manage Spotify Audio Player
+ *
+ * Created by kaushalmandayam on 4/29/17.
+ */
+
+public enum AudioPlayerManager
+{
+    INSTANCE;
+}
---
+++ DJRoomba/app/src/main/java/com/kaushalmandayam/djroomba/managers/LoginManager.java
@@ -0,0 +1,11 @@
+package com.kaushalmandayam.djroomba.managers;
+
+/**
+ * Manager to handle login requests
+ *
+ * Created by kaushalmandayam on 4/29/17.
+ */
+
+public class LoginManager
+{
+}
